2c4d5019ef2acb9253254cef2fe481070d4f53e3
Andrzej Klajnert <github@aklajnert.pl>
2020-10-21 01:16:24 +0200

    Allow to save multiple positions (#2)
    * Load positions dynamically from config
    
    * Implement save and delete methods
    
    * Fixed tests
    
    * Add newlines to stdout.write calls.
    
    * Do not allow position with the same name as reserved names.
    
    Co-authored-by: Alex M <alexmgit@protonmail.com>

--- idasen/cli.py
+++ idasen/cli.py
@@ -1,3 +1,5 @@
+import functools
+
 from . import IdasenDesk
 from typing import Callable
 from typing import List
@@ -10,33 +12,37 @@ import sys
 import voluptuous as vol
 import yaml
 
-home = os.path.expanduser("~")
-idasen_config_directory = os.path.join(home, ".config", "idasen")
-idasen_config_path = os.path.join(idasen_config_directory, "idasen.yaml")
+HOME = os.path.expanduser("~")
+IDASEN_CONFIG_DIRECTORY = os.path.join(HOME, ".config", "idasen")
+IDASEN_CONFIG_PATH = os.path.join(IDASEN_CONFIG_DIRECTORY, "idasen.yaml")
 
-default_config = {
-    "stand_height": 1.1,
-    "sit_height": 0.75,
+DEFAULT_CONFIG = {
+    "positions": {"stand": 1.1, "sit": 0.75},
     "mac_address": "AA:AA:AA:AA:AA:AA",
 }
 
-config_schema = vol.Schema(
+CONFIG_SCHEMA = vol.Schema(
     {
         "mac_address": vol.All(str, vol.Length(min=17, max=17)),
-        "stand_height": vol.All(
-            vol.Any(float, int),
-            vol.Range(min=IdasenDesk.MIN_HEIGHT, max=IdasenDesk.MAX_HEIGHT),
-        ),
-        "sit_height": vol.All(
-            vol.Any(float, int),
-            vol.Range(min=IdasenDesk.MIN_HEIGHT, max=IdasenDesk.MAX_HEIGHT),
-        ),
+        "positions": {
+            str: vol.All(
+                vol.Any(float, int),
+                vol.Range(min=IdasenDesk.MIN_HEIGHT, max=IdasenDesk.MAX_HEIGHT),
+            )
+        },
     },
     extra=False,
 )
 
+RESERVED_NAMES = {"init", "monitor", "height", "save", "delete"}
 
-def load_config(path: str = idasen_config_path) -> dict:
+
+def save_config(config: dict, path: str = IDASEN_CONFIG_PATH):
+    with open(path, "w") as f:
+        yaml.dump(config, f)
+
+
+def load_config(path: str = IDASEN_CONFIG_PATH) -> dict:
     """ Load user config. """
     try:
         with open(path, "r") as f:
@@ -44,11 +50,33 @@ def load_config(path: str = idasen_config_path) -> dict:
     except FileNotFoundError:
         return {}
 
+    # convert old config file format
+    if "positions" not in config:
+        config["positions"] = {}
+        config["positions"]["sit"] = config.pop(
+            "sit_height", DEFAULT_CONFIG["positions"]["sit"]
+        )
+        config["positions"]["stand"] = config.pop(
+            "stand_height", DEFAULT_CONFIG["positions"]["stand"]
+        )
+
+        save_config(config, path)
+
     try:
-        return config_schema(config)
+        config = CONFIG_SCHEMA(config)
     except vol.Invalid as e:
         sys.stderr.write(f"Invalid configuration: {e}\n")
         sys.exit(1)
+    else:
+        for position in config["positions"]:
+            if position in RESERVED_NAMES:
+                sys.stderr.write(
+                    "Invalid configuration, "
+                    f"position with name '{position}' is a reserved name.\n"
+                )
+                sys.exit(1)
+
+        return config
 
 
 def add_common_args(parser: argparse.ArgumentParser):
@@ -63,23 +91,22 @@ def add_common_args(parser: argparse.ArgumentParser):
     )
 
 
-def get_parser() -> argparse.ArgumentParser:
+def get_parser(config: dict) -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(description="ikea IDÅSEN desk control")
     sub = parser.add_subparsers(dest="sub", help="Subcommands", required=True)
 
     height_parser = sub.add_parser("height", help="Get the desk height.")
     monitor_parser = sub.add_parser("monitor", help="Monitor the desk position.")
-    sit_parser = sub.add_parser("sit", help="Move the desk to a sitting position.")
-    stand_parser = sub.add_parser("stand", help="Move the desk to a standing position.")
     init_parser = sub.add_parser("init", help="Initialize a new configuration file.")
+    save_parser = sub.add_parser("save", help="Save current desk position.")
+    save_parser.add_argument("name", help="Position name")
+    delete_parser = sub.add_parser("delete", help="Remove position with given name.")
+    delete_parser.add_argument("name", help="Position name")
 
-    stand_parser.add_argument(
-        "--stand-height", type=float, help="Standing height in meters."
-    )
-
-    sit_parser.add_argument(
-        "--sit-height", type=float, help="Sitting height in meters."
-    )
+    positions = config.get("positions", {})
+    for name, value in positions.items():
+        subcommand = sub.add_parser(name, help=f"Move the desk to {value}m.")
+        add_common_args(subcommand)
 
     init_parser.add_argument(
         "-f",
@@ -90,14 +117,14 @@ def get_parser() -> argparse.ArgumentParser:
 
     add_common_args(height_parser)
     add_common_args(monitor_parser)
-    add_common_args(sit_parser)
-    add_common_args(stand_parser)
+    add_common_args(save_parser)
+    add_common_args(delete_parser)
 
     return parser
 
 
 async def init(args: argparse.Namespace) -> int:
-    if not args.force and os.path.isfile(idasen_config_path):
+    if not args.force and os.path.isfile(IDASEN_CONFIG_PATH):
         sys.stderr.write("Configuration file already exists.\n")
         sys.stderr.write("Use --force to overwrite existing configuration.\n")
         return 1
@@ -105,21 +132,21 @@ async def init(args: argparse.Namespace) -> int:
         mac = await IdasenDesk.discover()
         if mac is not None:
             sys.stderr.write(f"Discovered desk's MAC address: {mac}")
-            default_config["mac_address"] = mac
+            DEFAULT_CONFIG["mac_address"] = mac
         else:
             sys.stderr.write("Failed to discover desk's MAC address")
-        os.makedirs(idasen_config_directory, exist_ok=True)
-        with open(idasen_config_path, "w") as f:
+        os.makedirs(IDASEN_CONFIG_DIRECTORY, exist_ok=True)
+        with open(IDASEN_CONFIG_PATH, "w") as f:
             f.write(
                 "# https://idasen.readthedocs.io/en/latest/index.html#configuration\n"
             )
-            yaml.dump(default_config, f)
-        sys.stderr.write(f"Created new configuration file at: {idasen_config_path}")
+            yaml.dump(DEFAULT_CONFIG, f)
+        sys.stderr.write(f"Created new configuration file at: {IDASEN_CONFIG_PATH}")
 
     return 0
 
 
-async def monitor(args: argparse.Namespace) -> int:
+async def monitor(args: argparse.Namespace) -> None:
     try:
         async with IdasenDesk(args.mac_address) as desk:
             previous_height = 0.0
@@ -139,14 +166,38 @@ async def height(args: argparse.Namespace):
         sys.stdout.write(f"{height:.3f} meters\n")
 
 
-async def stand(args: argparse.Namespace):
+async def move_to(args: argparse.Namespace, position: float) -> None:
     async with IdasenDesk(args.mac_address) as desk:
-        await desk.move_to_target(target=args.stand_height)
+        await desk.move_to_target(target=position)
 
 
-async def sit(args: argparse.Namespace):
+async def save(args: argparse.Namespace, config: dict) -> int:
+    if args.name in RESERVED_NAMES:
+        sys.stderr.write(f"Position with name '{args.name}' is a reserved name.\n")
+        return 1
+
     async with IdasenDesk(args.mac_address) as desk:
-        await desk.move_to_target(target=args.sit_height)
+        height = await desk.get_height()
+
+    config["positions"][args.name] = height
+    save_config(config)
+
+    sys.stdout.write(f"Saved position '{args.name}' with height: {height}m.\n")
+    return 0
+
+
+async def delete(args: argparse.Namespace, config: dict) -> int:
+    position = config["positions"].pop(args.name, None)
+    if args.name in RESERVED_NAMES:
+        sys.stderr.write(f"Position with name '{args.name}' is a reserved name.\n")
+        return 1
+    elif position is None:
+        sys.stderr.write(f"Position with name '{args.name}' doesn't exist.\n")
+    else:
+        save_config(config)
+        sys.stdout.write(f"Position with name '{args.name}' removed.\n")
+
+    return 0
 
 
 def from_config(
@@ -172,29 +223,30 @@ def count_to_level(count: int) -> int:
     return logging.CRITICAL
 
 
-def subcommand_to_callable(sub: str) -> Callable:
+def subcommand_to_callable(sub: str, config: dict) -> Callable:
     if sub == "init":
         return init
     elif sub == "monitor":
         return monitor
-    elif sub == "sit":
-        return sit
     elif sub == "height":
         return height
-    elif sub == "stand":
-        return stand
+    elif sub == "save":
+        return functools.partial(save, config=config)
+    elif sub == "delete":
+        return functools.partial(delete, config=config)
+    elif sub in config.get("positions", {}):
+        position = config["positions"][sub]
+        return functools.partial(move_to, position=position)
     else:
         raise AssertionError(f"internal error, please report this bug {sub=}")
 
 
 def main(args: Optional[List[str]] = None):
-    parser = get_parser()
-    args = parser.parse_args(args)
     config = load_config()
+    parser = get_parser(config)
+    args = parser.parse_args(args)
 
     from_config(args, config, parser, "mac_address")
-    from_config(args, config, parser, "stand_height")
-    from_config(args, config, parser, "sit_height")
 
     level = count_to_level(args.verbose)
 
@@ -207,7 +259,7 @@ def main(args: Optional[List[str]] = None):
     root_logger.addHandler(handler)
     root_logger.setLevel(level)
 
-    func = subcommand_to_callable(args.sub)
+    func = subcommand_to_callable(args.sub, config)
 
     rc = asyncio.run(func(args))
 
