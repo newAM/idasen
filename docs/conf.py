3e445a63665fc94368046dcd1b47e7b3e3b2ac32
Alex Martens <alex@thinglab.org>
2022-08-28 17:13:37 -0700

    docs: fix copyright footer

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,7 @@ master_doc = "index"
 # General information about the project.
 project = pyproject["tool"]["poetry"]["name"]
 year = datetime.datetime.now().year
-author = pyproject["tool"]["poetry"]["authors"][0]
+author = pyproject["tool"]["poetry"]["authors"][0].split("<", 1)[0].rstrip()
 copyright = f"{year}, {author}"
 version = pyproject["tool"]["poetry"]["version"]
 release = pyproject["tool"]["poetry"]["version"]
